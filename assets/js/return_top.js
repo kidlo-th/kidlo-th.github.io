ebd9c30eb037855edb07f7363abf96e5713296f2
kidlo-th <x_weiser@163.com>
2024-05-31 14:23:36 +0800

    Update return_top.js

--- assets/js/return_top.js
+++ assets/js/return_top.js
@@ -7,17 +7,15 @@ function scrollFunction() {
 		document.getElementById("back-to-top-btn").style.display = "none";
 	}
 }
-// 返回顶部的功能（带动画效果）
+// 返回顶部
 function topFunctionArrow() {
 	// 获取当前滚动位置
 	var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
 
-	// 如果当前滚动位置大于0，则执行动画
 	if (currentScroll > 0) {
-		// 使用 requestAnimationFrame 实现平滑滚动效果
+		// requestAnimationFrame 实现平滑滚动
 		window.requestAnimationFrame(topFunctionArrow);
-		// 将滚动位置向上移动一定距离，这里使用了一个简单的缓动函数
-		// 你也可以使用其他更复杂的缓动函数来实现更自然的动画效果
+		// 将滚动位置向上移动一定距离
 		window.scrollTo(0, currentScroll - (currentScroll / 10));
 	}
 }
